d14b5c88f934cccadbcc886b09bde21f69ff0911
statiolake <statiolake@gmail.com>
2022-01-09 00:34:13 +0900

    fix: set default database server to sqlite3

--- backend/apiserver/settings.py
+++ backend/apiserver/settings.py
@@ -147,18 +147,10 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
